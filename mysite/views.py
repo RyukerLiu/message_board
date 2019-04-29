19734ccbca5256fca7bd7ab0c3d31a1534503e0f
Ryuker <alex03108861@yahoo.com.tw>
2019-04-29 00:23:22 -0700

    ver2

--- mysite/views.py
+++ mysite/views.py
@@ -21,3 +21,27 @@ def index(request):
 		message='儲存成功! 請記得你的編輯密碼 [{}]!，訊息審查後才會顯示。'.format(user_pass)
 		
 	return render(request, 'index.html', locals())
+
+def listing(request):
+	posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:150]
+	moods = models.Mood.objects.all()
+	return render(request, 'listing.html', locals())
+
+def posting(request):
+	moods = models.Mood.objects.all()
+	try:
+		user_id = request.GET['user_id']
+		user_pass = request.GET['user_pass']
+		user_post = request.GET['user_post']
+		user_mood = request.GET['mood']
+	except:
+		user_id = None
+		message = '如要張貼訊息，則每一欄都要填寫...'
+	
+	if user_id != None:
+		mood = models.Mood.objects.get(status=user_mood)
+		post = models.Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+		post.save()
+		message='儲存成功! 請記得你的編輯密碼 [{}]!，訊息審查後才會顯示。'.format(user_pass)
+		
+	return render(request, 'posting.html', locals())
