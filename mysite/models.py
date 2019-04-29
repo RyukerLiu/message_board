0a83f73c18efa636a36313cd17dadb21c42ad7bb
Ryuker <alex03108861@yahoo.com.tw>
2019-04-28 23:42:38 -0700

    test1

---
+++ mysite/models.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from django.db import models
+
+class Mood(models.Model):
+    status = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return self.status
+
+class Post(models.Model):
+    mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=10, default='Anonym')
+    message = models.TextField(null=False)
+    del_pass = models.CharField(max_length=10)
+    pub_time = models.DateTimeField(auto_now=True)
+    enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.message
---
+++ mysite/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from mysite import models
+
+# Create your views here.
+def index(request):
+	posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+	moods = models.Mood.objects.all()
+	try:
+		user_id = request.GET['user_id']
+		user_pass = request.GET['user_pass']
+		user_post = request.GET['user_post']
+		user_mood = request.GET['mood']
+	except:
+		user_id = None
+		message = '如要張貼訊息，則每一欄都要填寫...'
+	
+	if user_id != None:
+		mood = models.Mood.objects.get(status=user_mood)
+		post = models.Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+		post.save()
+		message='儲存成功! 請記得你的編輯密碼 [{}]!，訊息審查後才會顯示。'.format(user_pass)
+		
+	return render(request, 'index.html', locals())
